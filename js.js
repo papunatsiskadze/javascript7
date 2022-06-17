c465c1218168a01701b7a3e37d6369cc7ef620aa
papunatsiskadze <papunaciskadze@gmail.com>
2022-06-17 14:51:07 +0400

    commit

--- js.js
+++ js.js
@@ -2,13 +2,21 @@ const draggables = document.querySelectorAll('.draggable')
 const containers = document.querySelectorAll('.container')
 
 draggables.forEach(draggable => {
-    draggable.addEventListener('drag start',() => {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging')
         console.log('Drag Start')
+    })
+    
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging')
+        console.log('Drop')
     })
 })
 
 containers.forEach(container => {
-    container.addEventListener('drag over', () => {
-        console.log('drag over')
+    container.addEventListener('dragover', () => {
+        console.log('Drag Over')
+        const draggable = document.querySelector('.dragging')
+        container.appendChild(draggable)
     })
 })
